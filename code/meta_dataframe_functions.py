ea2a4cc9ed66695ac7aaca3ac8b3803388a4682d
Jenna Epstein <epstein.jenna@gmail.com>
2025-09-26 13:24:41 +0200

    move meta_df functions to file

---
+++ code/meta_dataframe_functions.py
@@ -0,0 +1,79 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import re
+
+import qiskit
+from qiskit.providers.fake_provider import GenericBackendV2
+from qiskit_ibm_runtime import SamplerV2 as Sampler
+from qiskit_ibm_runtime.fake_provider import FakeManilaV2, FakeTorino, FakeBrisbane
+from qiskit_ibm_runtime.fake_provider import FakeFez, FakeMarrakesh #fez, marrakesh are both Heron2
+from qiskit_ibm_runtime import QiskitRuntimeService
+
+import data_extract_funcs
+
+def extract_cols_from_filename(file_name_str, dir_):
+    nr_qubits = re.findall(r'\d+',file_name_str[0:3])[0]
+    backend_name_start = len(nr_qubits)+2 #might need to 7 change if 'ibm_..." is included in file name"
+    circuit_type = file_name_str[-5]
+    is_sim = False
+    
+    if file_name_str.find("fake")!=-1:
+        is_sim = True
+    if is_sim:
+        backend_name_start = backend_name_start +5
+    else:
+        backend_name_start = backend_name_start +4
+        
+    backend_name = file_name_str[backend_name_start:-5]
+
+    file_path = dir_+file_name_str
+    
+    return [nr_qubits,backend_name,is_sim,circuit_type,file_path]
+
+def add_file_to_meta_df(meta_df,file_name,dir_):
+    row = extract_cols_from_filename(file_name,dir_)
+    meta_df.loc[len(meta_df)] = row
+    
+def add_qfolder_to_df(df,folder_dir,backends_,nr_qubits):
+    for backend in backends_:
+        backend_name = backend.backend_name
+        file_names = data_extract_funcs.make_file_names(backend_name,nr_qubits)
+        for file_name in file_names:
+            add_file_to_meta_df(df,file_name,folder_dir)
+
+def add_qfolders(df,backends,folders):
+    for folder in folders:
+        nr_qubits = re.findall(r'\d+',folder)[0]
+        add_qfolder_to_df(df,folder,backends,nr_qubits)
+
+def blank_meta_df():
+    meta_df = pd.DataFrame(columns = ['nr_qubits','backend','sim','circuit_type','file_path'])
+    return meta_df
+
+def load_meta_df(meta_df,experiment_type):#Hardware,Simulation,Refreshed_Simulation
+    dir_Hardware = ["../Hardware_results/4q/","../Hardware_results/8q/"]
+    dir_Sims = ["../Simulated_results/4q/","../Simulated_results/8q/","../Simulated_results/16q/"]
+    dir_Refr_Sims = ["../Refreshed_Simulated_results/4q/","../Refreshed_Simulated_results/8q/","../Refreshed_Simulated_results/16q/"]
+    
+    service = QiskitRuntimeService()
+    fake_backends = [FakeTorino(), FakeFez(), FakeMarrakesh(),FakeBrisbane()]
+    hardware_backends = [service.backend('ibm_brisbane'), service.backend('ibm_torino')]
+    
+    match experiment_type:
+        case 'Hardware':
+            dir_ = dir_Hardware
+            backends_ = hardware_backends
+        case 'Simulation':
+            dir_ = dir_Sims
+            backends_ = fake_backends
+        case 'Refreshed_Simulation':
+            dir_ = dir_Refr_Sims
+            backends_ = fake_backends
+
+    add_qfolders(meta_df,backends_,dir_)
+
+def get_results_df_from_row(row_index,meta_df):
+    csv_file = meta_df.loc[row_index,'file_path']
+    csv_df = pd.read_csv(csv_file)
+    return csv_df
