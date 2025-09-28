ad05335ee6930b15d0be964e844b9f9c60b3dce2
CraftierLamb599 <rameez.abdool@gmail.com>
2025-09-28 14:30:23 +0200

    seperate array of complete and move total errors to the front

--- code/data_process_funcs.py
+++ code/data_process_funcs.py
@@ -22,7 +22,8 @@ def create_processed_df(file_name, shots = 4096):
 
     totalErrors = df2.iloc[:,0]
     df2 = df2.div(totalErrors, axis=0)
-    df2['totalError'] = totalErrors
+    # df2['totalError'] = totalErrors
+    df2.insert(loc=0, column='totalError', value=totalErrors)
     return df2
 
 def create_processed_dfs(file_names_array, shots = 4096):
@@ -32,12 +33,12 @@ def create_processed_dfs(file_names_array, shots = 4096):
         dfs.append(df)
     return dfs
 
-def get_meta_dataframe(type_data, nr_qubit_types = 2,nr_machines = 2, nr_circuits = 3):
+def get_meta_dataframe(type_data, nr_qubit_sizes = 2,nr_machines = 2, nr_circuits = 3):
     bigDf =  meta_dataframe_functions.blank_meta_df()
     meta_dataframe_functions.load_meta_df(bigDf,type_data)
 
     df_array = []
-    for i in range (nr_machines * nr_circuits * nr_qubit_types):
+    for i in range (nr_machines * nr_circuits * nr_qubit_sizes):
         df = create_processed_df(bigDf['file_path'][i])
         df.insert(loc=0, column='circuit_type', value=bigDf['circuit_type'][i])
         df.insert(loc=0, column='backend', value=bigDf['backend'][i])
@@ -51,3 +52,14 @@ def join_dfs(dfs):
     for df in dfs:
         bigDf = pd.concat([bigDf, df], ignore_index=True)
     return bigDf
+
+def arr_dfs_of_qubit_sizes(type_data, nr_qubit_sizes = 2,nr_machines = 2, nr_circuits = 3):
+    df_arr = get_meta_dataframe(type_data, nr_qubit_sizes, nr_machines, nr_circuits)
+    df = []
+    for i in range (nr_qubit_sizes):
+        start_index = i * nr_machines * nr_circuits
+        end_index = (i + 1) * nr_machines * nr_circuits
+        df_size = join_dfs(df_arr[start_index:end_index])
+        df.append(df_size)
+    return df
+    
