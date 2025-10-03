897c1dddd05b9fd5cb5d395fff426af43ee53e53
Jenna Epstein <epstein.jenna@gmail.com>
2025-10-03 19:20:05 +0200

    fix train on full X,Y when testing against other dfs

--- code/ml_funcs.py
+++ code/ml_funcs.py
@@ -221,7 +221,7 @@ def make_pairs(indiv_dfs):
 
     return pair_dfs
 
-def get_accuracies_for_comparison(model, tr_val_dfp, tr_label,test_dfps, test_dfp_labels, to_print = False):
+def get_accuracies_for_comparison(model, tr_val_dfp, tr_label,test_dfps, test_dfp_labels, to_print = False, get_self_score = True):
     
     test_scores = []
     labels =[]
@@ -231,14 +231,20 @@ def get_accuracies_for_comparison(model, tr_val_dfp, tr_label,test_dfps, test_df
     X_tr_val,Y_tr_val = get_x_y(tr_val_dfp)
     X_train_self, X_test_self, Y_train_self, Y_test_self = model_selection.train_test_split(
     X_tr_val,Y_tr_val,test_size=0.2,shuffle = True,random_state=42)
-    fitted_model, self_score = fit_and_get_score(
-    model,X_train_self,Y_train_self,X_test_self,Y_test_self)
+    if get_self_score:
+        fitted_model, self_score = fit_and_get_score(
+        model,X_train_self,Y_train_self,X_test_self,Y_test_self)
+    
+    Y_tr_val_1d = Y_tr_val.to_numpy()
+    Y_tr_val_1d = Y_tr_val_1d.ravel()
+
+    fitted_model_full = model.fit(X_tr_val,Y_tr_val_1d)
 
     test_scores.append(self_score)
 
     for dfp in test_dfps:
         X,Y = get_x_y(dfp)
-        test_score = fitted_model.score(X, Y) #check score vs accurcy_score
+        test_score = fitted_model_full.score(X, Y) #check score vs accurcy_score
         test_scores.append(test_score)
 
     if to_print:
