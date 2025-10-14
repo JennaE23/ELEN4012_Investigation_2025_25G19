1afd3e801089ff0feec04aeb35be495b28a15b4c
Jenna Epstein <epstein.jenna@gmail.com>
2025-10-14 11:13:23 +0200

    add cluster code file

---
+++ code/cluster_ml2csv_all.py
@@ -0,0 +1,40 @@
+
+from investigation_functions import  ml_to_csv_funcs as ml2csv
+from investigation_functions import  ml_funcs as mlf
+from investigation_functions import  test_table_funcs as ttf
+import backend_vars
+from ml_models_vars import Param_Modes
+
+from itertools import combinations
+
+qubits_list = backend_vars.qubits_list
+svm_modes = Param_Modes().SVM_modes
+knn_modes_all_k = Param_Modes().KNN_modes
+knn_5_modes = []
+for knn_modes in knn_modes_all_k:
+    knn_5_modes.append(knn_modes[0])
+
+
+dir_runs = ""
+dir_ml = "ML_results/SVM/"
+file_name = "svm_16q.csv"
+
+backend_list = ['brisbane','torino','fez','marrakesh']
+backend_combos = list(combinations(backend_list, 2))
+herons = tuple(['torino','fez','marrakesh'])
+backend_combos.append(herons)
+
+#to be changed if changing betw SVM and KNN:
+param_modes = svm_modes
+param_modes_best_H_S_R=param_modes[2:5]
+ml2csv.create_ml_results_csv('SVM',dir_ml)
+
+for qubit_nr in [4]:#qubits_list:
+    initial_list = ttf.get_HSR_array_all_backends(qubit_nr,dir_runs, True)
+    
+    ml2csv.run_and_record_HSR_c111(initial_list,dir_ml,file_name,param_modes)
+    ml2csv.run_and_record_HSR_for_each_c_type(initial_list, dir_ml, file_name, param_modes)
+    ml2csv.run_and_record_circuit_test_table(initial_list, dir_ml, file_name, param_modes)
+    ml2csv.run_and_record_backends_v_backends(
+        initial_list, dir_ml, file_name,param_modes_best_H_S_R, backend_combos
+    )
